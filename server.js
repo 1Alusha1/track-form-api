42b6d6f43adbbd3a66845986e09c5e24af7dbac3
1alusha1 <alusha9636@gmail.com>
2025-03-27 21:46:22 +0200

    Added several user routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express";
-import { config } from "dotenv";
-import cors from "cors";
-import mongoose from "mongoose";
-import LeadSchema from "./models/Lead.js";
+import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import LeadSchema from './models/Lead.js';
+import UserSchema from './models/User.js';
 
 config();
 
@@ -12,11 +13,12 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ hello: "world" });
+app.get('/', (req, res) => {
+  res.status(200).json({ hello: 'world' });
 });
 
-app.post("/", async (req, res) => {
+// lead
+app.post('/', async (req, res) => {
   const { email, platform, userId } = req.body;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -35,9 +37,9 @@ app.post("/", async (req, res) => {
     const response = await fetch(
       `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${userId}&text=potential lead clicked on platform: ${platform}`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "content-type": "application/json",
+          'content-type': 'application/json',
         },
       }
     );
@@ -47,7 +49,7 @@ app.post("/", async (req, res) => {
     if (data.ok) {
       return res.status(201).json({
         error: false,
-        message: "new lead click has benn added and sended",
+        message: 'new lead click has benn added and sended',
       });
     }
   } catch (err) {
@@ -55,20 +57,86 @@ app.post("/", async (req, res) => {
 
     return res
       .status(500)
-      .json({ error: true, message: "Error while adding record in database" });
+      .json({ error: true, message: 'Error while adding record in database' });
   }
 });
 
-app.get("/get-leads", async (req, res) => {
+app.get('/get-leads', async (req, res) => {
   try {
     const records = await LeadSchema.findOne();
 
-    return res.status(200).json({ error: false, message: "", records });
+    return res.status(200).json({ error: false, message: '', records });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
       error: err,
-      message: "Error while geting records from database",
+      message: 'Error while geting records from database',
+    });
+  }
+});
+
+// user
+app.post('/register-user', async (req, res) => {
+  const { userId, username, first_name } = req.body;
+
+  try {
+    if (!userId && !username && !first_name) {
+      return res.status(400).json({
+        error: err,
+        message: 'Fields: userId, user_name, full_name are required ',
+      });
+    }
+
+    const record = await UserSchema.findOne({ userId });
+
+    if (!record) {
+      return res.status(400).json({
+        error: true,
+        message: `You've already registred`,
+      });
+    }
+
+    const newUser = await new UserSchema({ userId, username, first_name });
+
+    if (newUser) {
+      newUser.save();
+    }
+    return res.status(201).json({
+      error: false,
+      message: 'User record has got successfully created',
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+      message: 'Error while adding user records to database',
+    });
+  }
+});
+
+app.get('/get-user/:userId', async (req, res) => {
+  const { userId } = req.params;
+  console.log(userId);
+  try {
+    if (!userId) {
+      return res.status(400).json({
+        error: false,
+        message: 'Field: userId is required ',
+      });
+    }
+
+    const record = await UserSchema.findOne({ userId });
+
+    res.status(200).json({
+      error: false,
+      message: '',
+      record,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+      message: 'Error while getting user record from database',
     });
   }
 });
@@ -78,9 +146,9 @@ app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI, {})
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
-app.listen(process.env.PORT, () => console.log("server was start"));
+app.listen(process.env.PORT, () => console.log('server was start'));
 
 export default app;
