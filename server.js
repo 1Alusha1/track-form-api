916ef00eb2c176b08cf6e995f835f3fe46ba1380
1alusha1 <alusha9636@gmail.com>
2025-04-01 12:12:24 +0300

    added route to get leads by date

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import { config } from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import LeadSchema from './models/Lead.js';
-import UserSchema from './models/User.js';
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import LeadSchema from "./models/Lead.js";
+import UserSchema from "./models/User.js";
 
-import { formatDateTimeManualUTC2 } from './utils/formatDateTime.js';
+import { formatDateTimeManualUTC2 } from "./utils/formatDateTime.js";
+import { getDatePointGMT } from "./utils/getDatePoin.js";
 
 config();
 
@@ -15,12 +16,12 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ hello: 'world' });
+app.get("/", (req, res) => {
+  res.status(200).json({ hello: "world" });
 });
 
 // lead
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   const { email, platform, userId, utmLink } = req.body;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -42,15 +43,15 @@ app.post('/', async (req, res) => {
       `https://api.telegram.org/bot${
         process.env.BOT_TOKEN
       }/sendMessage?chat_id=${userId}&text=
-        ${platform ? `Platform: ${platform} ` : ''} \n
-        ${email ? `Email: ${email}` : ''}\n
-        ${utmLink ? `UTM: ${JSON.stringify(utmLink)}` : ''}\n
+        ${platform ? `Platform: ${platform} ` : ""} \n
+        ${email ? `Email: ${email}` : ""}\n
+        ${utmLink ? `UTM: ${JSON.stringify(utmLink)}` : ""}\n
         ${formatDateTimeManualUTC2(lead.created_at)}
       `,
       {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'content-type': 'application/json',
+          "content-type": "application/json",
         },
       }
     );
@@ -62,7 +63,7 @@ app.post('/', async (req, res) => {
     if (data.ok) {
       return res.status(201).json({
         error: false,
-        message: 'new lead click has benn added and sended',
+        message: "new lead click has benn added and sended",
       });
     }
   } catch (err) {
@@ -70,33 +71,81 @@ app.post('/', async (req, res) => {
 
     return res
       .status(500)
-      .json({ error: true, message: 'Error while adding record in database' });
+      .json({ error: true, message: "Error while adding record in database" });
   }
 });
 
-app.get('/get-leads', async (req, res) => {
+app.get("/get-leads", async (req, res) => {
   try {
     const records = await LeadSchema.findOne();
 
-    return res.status(200).json({ error: false, message: '', records });
+    return res.status(200).json({ error: false, message: "", records });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
       error: err,
-      message: 'Error while geting records from database',
+      message: "Error while geting records from database",
+    });
+  }
+});
+
+app.post("/get-leads-by-date", async (req, res) => {
+  console.log(req.body);
+  const { date } = req.body;
+  const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+
+  if (!dateRegex.test(date)) {
+    return res.status(400).json({
+      error: true,
+      message: "Date has incorrect formate. Date mus be in format yyyy-mm-dd",
+    });
+  }
+  const dateArray = date.split("-");
+  console.log(dateArray);
+  const { startDay, endDay } = getDatePointGMT(
+    Number(dateArray[0]),
+    Number(dateArray[1]),
+    Number(dateArray[2])
+  );
+  console.log(startDay, endDay);
+  try {
+    const records = await LeadSchema.find({
+      created_at: { $gte: startDay, $lt: endDay },
+    });
+
+    if (!records.length) {
+      return res.status(200).json({
+        error: false,
+        message: "For this day leads haven't found",
+        records: [],
+        count: 0,
+      });
+    }
+
+    return res.status(200).json({
+      error: false,
+      message: "",
+      records: records,
+      count: records.length,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+      message: "Error while geting records by date from database",
     });
   }
 });
 
 // user
-app.post('/register-user', async (req, res) => {
+app.post("/register-user", async (req, res) => {
   const { userId, username, first_name } = req.body;
 
   try {
     if (!userId && !username && !first_name) {
       return res.status(400).json({
         error: err,
-        message: 'Fields: userId, user_name, full_name are required ',
+        message: "Fields: userId, user_name, full_name are required ",
       });
     }
 
@@ -116,25 +165,25 @@ app.post('/register-user', async (req, res) => {
     }
     return res.status(201).json({
       error: false,
-      message: 'User record has got successfully created',
+      message: "User record has got successfully created",
     });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
       error: err,
-      message: 'Error while adding user records to database',
+      message: "Error while adding user records to database",
     });
   }
 });
 
-app.get('/get-user/:userId', async (req, res) => {
+app.get("/get-user/:userId", async (req, res) => {
   const { userId } = req.params;
   console.log(userId);
   try {
     if (!userId) {
       return res.status(400).json({
         error: false,
-        message: 'Field: userId is required ',
+        message: "Field: userId is required ",
       });
     }
 
@@ -142,14 +191,14 @@ app.get('/get-user/:userId', async (req, res) => {
 
     res.status(200).json({
       error: false,
-      message: '',
+      message: "",
       record,
     });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
       error: err,
-      message: 'Error while getting user record from database',
+      message: "Error while getting user record from database",
     });
   }
 });
@@ -159,9 +208,9 @@ app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI, {})
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.listen(process.env.PORT, () => console.log('server was start'));
+app.listen(3000, () => console.log("server was start"));
 
 export default app;
